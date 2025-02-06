b239f9fef3c543235cc11a8cb0196e2f927f4d14
ramya72002 <nsriramya7@gmail.com>
2025-02-06 12:01:28 +0530

    gg

--- app.py
+++ app.py
@@ -686,7 +686,7 @@ def users_without_admin():
     users_list = [user for user in users]
     # Convert MongoDB documents to a list of dictionaries
  
-    users_list = [user for user in users_list if user.get('Admin') == True ]
+    users_list = [user for user in users_list if not user.get('Admin') == True ]
 
     return jsonify(users_list), 200
 
